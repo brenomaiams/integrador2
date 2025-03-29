8ead783f6cf620383bcc358d0e30aed1afe8e38e
Breno Maia da Silva Huchit <brenomaia.dev@hotmail.com>
2025-03-28 21:53:33 -0300

    last

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
-// Angular Material
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
-import { MatCheckboxModule } from '@angular/material/checkbox'; // Importar MatCheckboxModule
-
+import { MatCheckboxModule } from '@angular/material/checkbox'; 
+import { MatRadioModule } from '@angular/material/radio'; 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListaClientesComponent } from './features/clientes/lista/lista-clientes/lista-clientes.component';
 import { CadastrarComponent } from './cliente/cadastrar/cadastrar.component';
@@ -26,14 +28,17 @@ import { CadastrarComponent } from './cliente/cadastrar/cadastrar.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    // Angular Material
+
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatSnackBarModule,
     MatCardModule,
     MatInputModule,
     MatTableModule,
-    MatCheckboxModule // Adicione esta linha
+    FormsModule,
+    MatCheckboxModule, 
+    MatRadioModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cliente/cadastrar/cadastrar.component.ts
+++ src/app/cliente/cadastrar/cadastrar.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-cadastrar',
   standalone: false,
   templateUrl: './cadastrar.component.html',
-  styleUrl: './cadastrar.component.scss'
+  styleUrls: ['./cadastrar.component.scss']
 })
 export class CadastrarComponent {
+  nome: string = '';
+  idade: number = 0;
+  aceitoTermos: boolean = false;
+  genero: string = '';
 
+  constructor(private snackBar: MatSnackBar) {}
+
+  salvar(form: any) {
+    console.log('Formulário válido:', form.valid); 
+    console.log('Dados do formulário:', form.value); 
+
+    if (form.valid) {
+      
+      console.log('Nome:', form.value.nome);
+      console.log('Idade:', form.value.idade);
+      console.log('Aceito os termos:', form.value.aceitoTermos);
+      console.log('Gênero:', form.value.genero);
+      this.openSnackBar('Dados salvos com sucesso!', 'Fechar');
+    } else {
+      this.openSnackBar('Preencha todos os campos corretamente!', 'Fechar');
+    }
+  }
+
+  openSnackBar(message: string, action: string) {
+    
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
 }
